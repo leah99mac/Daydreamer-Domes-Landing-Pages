9eaef368f743b00118bd905e4b90fb7c76580abc
leah99mac <leah.m.mackay@gmail.com>
2023-08-21 22:10:22 -0400

    new landing page

---
+++ sunset_boho_new/app.js
@@ -0,0 +1,7 @@
+const companyName = document.querySelector('#company-name')
+const quote = document.querySelector('.quote')
+const booking_bar = document.querySelector('.booking-bar')
+
+companyName.textContent = 'DAYDREAMER DOMES'
+quote.textContent = 'Come daydream with us'
+booking_bar.textContent = 'NOW BOOKING FOR AUGUST 2023'
