4101e25b31e9990fa63f6dfa6984b8fb3eeba0a5
leah99mac <leah.m.mackay@gmail.com>
2023-08-27 21:14:29 -0400

    style email add container

--- sunset_boho_new/app.js
+++ sunset_boho_new/app.js
@@ -1,7 +1,7 @@
-const companyName = document.querySelector('#company-name')
-const quote = document.querySelector('.quote')
-const booking_bar = document.querySelector('.booking-bar')
+document.getElementById("add-button").addEventListener("click", addEmailInfo)
 
-companyName.textContent = 'DAYDREAMER DOMES'
-quote.textContent = 'Come daydream with us'
-booking_bar.textContent = 'NOW BOOKING FOR AUGUST 2023'
+function addEmailInfo () {
+    const div = document.createElement('div')
+    div.innerHTML = '<div class="add-email"> <div class="cancel-button-container"><button id="cancel-button">X</button></div> <label id="first-name" class="first-name-label input-label" for="first-name">First Name:</label> <div class="input-box-container first-name-input"><input id="first-name" class="input-box" type="text" name="first-name" placeholder="First Name"></div> <label id="last-name" class="last-name-label input-label" for="last-name">Last Name:</label> <div class="input-box-container last-name-input"><input id="last-name" class="input-box" type="text" name="last-name" placeholder="Last Name"></div> <label id="email-name" class="email-label input-label" for="email-name">Email:</label> <div class="input-box-container email-input"><input id="email-name" class="input-box" type="text" name="email-name" placeholder="Email"></div> <div class="add-email-button-container"><button id="add-email-button">ADD ME TO THE LIST</button></div> </div>'
+    document.getElementById('email-box').appendChild(div);
+}
